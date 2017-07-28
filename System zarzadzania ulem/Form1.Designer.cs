511261ce6f5577b2661955d9ec0b5d60da9f49e2
Mateusz Kaczmarek <mk16939@gmail.com>
2017-07-28 22:33:30 +0200

    First

---
+++ System zarzadzania ulem/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace System_zarzadzania_ulem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.assignJob = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.shifts = new System.Windows.Forms.NumericUpDown();
+            this.workerBeeJob = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.nextShift = new System.Windows.Forms.Button();
+            this.report = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.shifts)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.assignJob);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.shifts);
+            this.groupBox1.Controls.Add(this.workerBeeJob);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(255, 94);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Przydział prac robotnicom";
+            // 
+            // assignJob
+            // 
+            this.assignJob.Location = new System.Drawing.Point(9, 59);
+            this.assignJob.Name = "assignJob";
+            this.assignJob.Size = new System.Drawing.Size(237, 23);
+            this.assignJob.TabIndex = 4;
+            this.assignJob.Text = "Przypisz tę pracę pszczole";
+            this.assignJob.UseVisualStyleBackColor = true;
+            this.assignJob.Click += new System.EventHandler(this.assignJob_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(157, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Zmiany";
+            // 
+            // shifts
+            // 
+            this.shifts.Location = new System.Drawing.Point(160, 33);
+            this.shifts.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.shifts.Name = "shifts";
+            this.shifts.Size = new System.Drawing.Size(86, 20);
+            this.shifts.TabIndex = 2;
+            this.shifts.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // workerBeeJob
+            // 
+            this.workerBeeJob.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.workerBeeJob.FormattingEnabled = true;
+            this.workerBeeJob.Items.AddRange(new object[] {
+            "Nauczanie pszczółek",
+            "Pielęgnacja jaj",
+            "Utrzymanie ula",
+            "Wytwarzanie miodu",
+            "Zbieranie nektaru",
+            "Patrol z żądłami"});
+            this.workerBeeJob.Location = new System.Drawing.Point(9, 32);
+            this.workerBeeJob.Name = "workerBeeJob";
+            this.workerBeeJob.Size = new System.Drawing.Size(145, 21);
+            this.workerBeeJob.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Zadanie robotnicy";
+            // 
+            // nextShift
+            // 
+            this.nextShift.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nextShift.Location = new System.Drawing.Point(275, 17);
+            this.nextShift.Name = "nextShift";
+            this.nextShift.Size = new System.Drawing.Size(138, 89);
+            this.nextShift.TabIndex = 1;
+            this.nextShift.Text = "Przepracuj następną zmianę";
+            this.nextShift.UseVisualStyleBackColor = true;
+            this.nextShift.Click += new System.EventHandler(this.nextShift_Click);
+            // 
+            // report
+            // 
+            this.report.Location = new System.Drawing.Point(13, 113);
+            this.report.Multiline = true;
+            this.report.Name = "report";
+            this.report.Size = new System.Drawing.Size(400, 188);
+            this.report.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(425, 313);
+            this.Controls.Add(this.report);
+            this.Controls.Add(this.nextShift);
+            this.Controls.Add(this.groupBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "System zarządzania ulem";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.shifts)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button assignJob;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown shifts;
+        private System.Windows.Forms.ComboBox workerBeeJob;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button nextShift;
+        private System.Windows.Forms.TextBox report;
+    }
+}
+
---
+++ System zarzadzania ulem/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace System_zarzadzania_ulem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            workerBeeJob.SelectedIndex = 0;
+            Worker[] workers = new Worker[4];
+            workers[0] = new Worker(new string[] { "Zbieranie nektaru", "Wytwarzanie miodu" }, 175);
+            workers[1] = new Worker(new string[] { "Pielęgnacja jaj", "Nauczanie pszczółek" }, 114);
+            workers[2] = new Worker(new string[] { "Utrzymywanie ula", "Patrol z żądłami" }, 149);
+            workers[3] = new Worker(new string[] { "Zbieranie nektaru", "Wytwarzanie miodu", "Pielęgnacja jaj", "Nauczanie pszczółek", "Utrzymanie ula", "Patrol z żądłami" }, 155);
+            queen = new Queen(workers, 275);
+        }
+
+
+        private Queen queen;
+
+        private void assignJob_Click(object sender, EventArgs e)
+        {
+            if (queen.AssignWork(workerBeeJob.Text, (int)shifts.Value) == false)
+                MessageBox.Show("Nie ma dostępnych robotnic do wykonania zadania '"
+                    + workerBeeJob.Text + "'", "Królowa pszczół mówi...");
+            else
+                MessageBox.Show("Zadanie '" + workerBeeJob.Text + "' będzie ukończone za "
+                    + shifts.Value + " zmiany", "Królowa pszczół mówi...");
+        }
+
+        private void nextShift_Click(object sender, EventArgs e)
+        {
+            report.Text = queen.WorkTheNextShift();
+        }
+    }
+}
